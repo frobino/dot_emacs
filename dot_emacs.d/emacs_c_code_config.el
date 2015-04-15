9fffc9b9d376f2622829ba408bf0a6c8db215e85
osso <osso@jessie.bredbandsbolaget.se>
2015-04-15 22:58:38 +0200

    [MILESTONE] fixed script to use company, semantic, and function-arg
    Signed-off-by: osso <osso@jessie.bredbandsbolaget.se>

--- dot_emacs.d/emacs_c_code_config.el
+++ dot_emacs.d/emacs_c_code_config.el
@@ -24,7 +24,8 @@
   (add-hook 'c-mode-hook 'semantic-mode)
   (add-hook 'c++-mode-hook 'semantic-mode))
 
-;; Alternative way (without check): 
+;; Alternative way (without check):
+;;
 ;; (require 'semantic)
 ;; (global-semanticdb-minor-mode 1)
 ;; (global-semantic-idle-completions-mode 1)
@@ -43,21 +44,47 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Autocompletion with company mode using semantics as backend
-;; (require 'cc-mode)
-;; (require 'semantic)
-;; (require 'company)
-;; (global-company-mode 1)
-;; (company-semantic 1)
+
+(with-library-for-c company
+  (add-hook 'after-init-hook 'global-company-mode)
+  ;; NOTE: the following "delete" is not necessary if we target the CEDET semantic backend.
+  ;; If we want to use the clang (or others backend) we have to delete company-semantic,
+  ;; otherwise company-complete will use company-semantic instead of company-clang,
+  ;; because it has higher precedence in company-backends.  
+  (delete 'company-semantic company-backends)
+  (add-hook 'c-mode-hook
+	    (lambda() (define-key c-mode-map (kbd "<backtab>") 'company-complete))
+	    )
+  (add-hook 'c++-mode-hook
+	    (lambda() (define-key c++-mode-map (kbd "<backtab>") 'company-complete))
+	    )
+  )
+
+;; NOTES about company:
+;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; NOTE: the following does not work!
-;; But if i use the require technique it does not shoot out error...
+;; function-args: showing an inline arguments hint for the C/C++ function at point.
 
-;; Function-args: showing an inline arguments hint for the C/C++ function at point.
+(with-library-for-c function-args
+  (fa-config-default)
+  (add-hook 'c-mode-hook
+	    (lambda() (define-key c-mode-map [C-tab] 'moo-complete))
+	    )
+  (add-hook 'c++-mode-hook
+	    (lambda() (define-key c++-mode-map [C-tab] 'moo-complete))
+	    )
+  ;;(define-key c-mode-map (kbd "C-c C-c") 'moo-complete)
+  ;;(eval-after-load 'c-mode '(define-key c-mode-map [tab] 'outline-cycle))
+  ;;(define-key c-mode-map  [C-tab] 'moo-complete)
+  ;;(define-key c++-mode-map  [C-tab] 'moo-complete)
+  )
 
+;; Alternative way (without check): 
+;;
 ;; (with-library-for-c function-args
 ;;   (fa-config-default)
 ;;   (define-key c-mode-map  [(control tab)] 'moo-complete)
@@ -65,13 +92,10 @@
 ;;   (define-key c-mode-map (kbd "M-o")  'fa-show)
 ;;   (define-key c++-mode-map (kbd "M-o")  'fa-show))
 
-(with-library-for-c function-args
-  (fa-config-default)
-  (define-key map  [(tab)] 'moo-complete)
-  ;;(eval-after-load 'c-mode '(define-key c-mode-map [tab] 'outline-cycle))
-  ;;(define-key c-mode-map  [C-tab] 'moo-complete)
-  ;;(define-key c++-mode-map  [C-tab] 'moo-complete)
-  )
+;; NOTES about function-args:
+;;
+;; fa-jump, moo-complete, moo-jump-local are AMAZING functions. Find good key mapping for
+;; them and remember to use them
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
