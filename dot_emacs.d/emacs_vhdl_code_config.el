f4da909f60a4d0e68d7a505cab3929efc91167ff
unknown <a502038@AF-2PZNN12.af.se>
2015-06-26 16:34:06 +0200

    Added emacs_vhdl_code_config.el as a "script"
    Support ctags/etags in vhdl-mode, so that we can jump both from
    instantiation to entity declaration (M-.) and viceversa (ctags-search).
    Other functions are used for updating TAGS
    
    Unfortunately, at the moment we have to load manually the .el file
    using M-x load-file

--- dot_emacs.d/emacs_vhdl_code_config.el
+++ dot_emacs.d/emacs_vhdl_code_config.el
@@ -1,40 +1,149 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Macro to shoot out error in case a library is not available
-(defmacro with-library-for-c (symbol &rest body)
+(defmacro with-library-for-vhdl (symbol &rest body)
   `(condition-case nil
        (progn
          (require ',symbol)
          ,@body)
 
-     (error (message (format "ERROR: I guess we don't have %s available. Check emacs_c_code_config.el" ',symbol))
+     (error (message (format "ERROR: I guess we don't have %s available. Check emacs_vhdl_code_config.el" ',symbol))
             nil)))
-(put 'with-library-for-c 'lisp-indent-function 1)
-
-;; Available C style:
-;; “gnu”: The default style for GNU projects
-;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
-;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
-;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
-;; “stroustrup”: What Stroustrup, the author of C++ used in his book
-;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
-;; “linux”: What the Linux developers use for kernel development
-;; “python”: What Python developers use for extension modules
-;; “java”: The default style for java-mode (see below)
-;; “user”: When you want to define your own style
-(setq
- c-default-style "linux" ;; set style to "linux"
-  )
+(put 'with-library-for-vhdl 'lisp-indent-function 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; ctags/etags management for VHDL (requires exuberant ctags in path)
+;; ctags.el --- Exuberant Ctags utilities for Emacs
+
+;; eventually add etags-update.el: a global minor mode that updates TAGS when saving a file.
+
+;; -- ;; (add-hook 'vhdl-mode-hook
+;; -- ;; 	  ;; code to be executed only in vhdl-mode
+;; -- ;; 	  (
+	   
+	   ;; The following code is taken from: 
+	   ;; URL: https://bitbucket.org/semente/ctags.el
+	   (defvar ctags-command "ctags -e -R ")
+
+	   (defun ctags ()
+	     (call-process-shell-command ctags-command nil "*Ctags*"))
+
+
+	   (defun ctags-find-tags-file ()
+	     "Recursively searches each parent directory for a file named
+              TAGS and returns the path to that file or nil if a tags file is
+              not found or if the buffer is not visiting a file."
+	     (progn
+	       (defun find-tags-file-r (path)
+		 "Find the tags file from current to the parent directories."
+		 (let* ((parent-directory (file-name-directory (directory-file-name path)))
+			(tags-file-name (concat (file-name-as-directory path) "TAGS")))
+		   (cond
+		    ((file-exists-p tags-file-name) (throw 'found tags-file-name))
+		    ((string= "/TAGS" tags-file-name) nil)
+		    (t (find-tags-file-r parent-directory)))))
+
+	       (if (buffer-file-name)
+		   (catch 'found
+		     (find-tags-file-r (file-name-directory buffer-file-name)))
+		 nil)))
+
+	   (defun ctags-set-tags-file ()
+	     "Uses `ctags-find-tags-file' to find a TAGS file. If found,
+              set 'tags-file-name' with its path or set as nil."
+	     (setq-default tags-file-name (ctags-find-tags-file)))
+
+	   (defun ctags-create-tags-table ()
+	     (interactive)
+	     (let* ((current-directory default-directory)
+		    (top-directory (read-directory-name
+				    "Top of source tree: " default-directory))
+		    (file-name (concat (file-name-as-directory top-directory) "TAGS")))
+	       (cd top-directory)
+	       (if (not (= 0 (ctags)))
+		   (message "Error creating %s!" file-name)
+		 (setq-default tags-file-name file-name)
+		 (message "Table %s created and configured." tags-file-name))
+	       (cd current-directory)))
+
+	   (defun ctags-update-tags-table ()
+	     (interactive)
+	     (let ((current-directory default-directory))
+	       (if (not tags-file-name)
+		   (message "Tags table not configured.")
+		 (cd (file-name-directory tags-file-name))
+		 (if (not (= 0 (ctags)))
+		     (message "Error updating %s!" tags-file-name)
+		   (message "Table %s updated." tags-file-name))
+		 (cd current-directory))))
+
+	   (defun ctags-create-or-update-tags-table ()
+	     "Create or update a tags table with `ctags-command'."
+	     (interactive)
+	     (if (not (ctags-set-tags-file))
+		 (ctags-create-tags-table)
+	       (ctags-update-tags-table)))
+
+
+	   (defun ctags-search ()
+	     "A wrapper for `tags-search' that provide a default input."
+	     (interactive)
+	     (let* ((symbol-at-point (symbol-at-point))
+		    (default (symbol-name symbol-at-point))
+		    (input (read-from-minibuffer
+			    (if (symbol-at-point)
+				(concat "Tags search (default " default "): ")
+			      "Tags search (regexp): "))))
+	       (if (and (symbol-at-point) (string= input ""))
+		   (tags-search default)
+		 (if (string= input "")
+		     (message "You must provide a regexp.")
+		   (tags-search input)))))
+;; -- ;;    )
+;; -- ;; 	  
+;; -- ;;  )
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; EDE project management mode
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; Undo tree mode
 
 (with-library-for-c ede
-  (add-hook 'c-mode-hook 'global-ede-mode)
-  (add-hook 'c++-mode-hook 'global-ede-mode)
+  (global-ede-mode)
   )
 
 ;; NOTEs:
