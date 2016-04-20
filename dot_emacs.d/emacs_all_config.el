14fcc708e4a8b76d38bcabe64f243a69051944d7
frobino <frobino@users.noreply.github.com>
2016-04-20 13:06:51 +0200

    [CODE - all] included config to scroll one line at the time

--- dot_emacs.d/emacs_all_config.el
+++ dot_emacs.d/emacs_all_config.el
@@ -54,6 +54,49 @@
     (add-to-list 'exec-path "c:/Program Files (x86)/Git/bin")
     )
 ;;  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scroll set up modifying scroll-conservatively variable
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Scroll up to this many lines, to bring point back on screen
+;;
+;; If the value is greater than 100, redisplay will never recenter point,
+;;    but will always scroll just enough text to bring point into view, even
+;;    if you move far away.
+;;
+;;    A value of zero means always recenter point if it moves off screen.
+(setq scroll-conservatively most-positive-fixnum)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup scratch buffer message
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Setup scratch message with "useful" informations,
+;; such as a reminder of the most important shortcuts
+;;
+;; (setq initial-scratch-message "\
+;; # Open buffer list: C-x C-b
+;; # Open buffer by name: C-x b
+;; # Switch frame: C-x o
+;; # Kill buffer: C-x k
+;; # Indent lines: C-x tab arrow
+;; # Open file C-x C-f
+;; # M-x replace-string <ENTER> <searchstr> <ENTER> <replstr>
+;; # C-a beginning of line
+;; # C-e end of line
+;; # C-v page forward
+;; # M-v page backward
+;; # S-Arrow go to next window
+;; # 
+;; # ---ETAGS---
+;; # Go to source code root
+;; # Create file with all tags: find . -regex \".*\(h$\|cpp$\)\" > files.txt
+;; # Create TAGS: path/to/etags.exe - < files.txt
+;; # Make TAGS acrtive in file: M-x visit-tags-table
+;; # Search functions: M-x find-tag
+;; ")
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Add golden ratio mode down here?
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
